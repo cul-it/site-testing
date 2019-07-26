fce52f64a1f7ccf1f5489b5aa0244107c2eb3b38
James Reidy <jreidy@cornell.edu>
2019-07-26 13:40:08 -0400

    show me the cookies script steps

--- features/step_definitions/custom_steps.rb
+++ features/step_definitions/custom_steps.rb
@@ -536,3 +536,14 @@ end
 Then("I should see the Staff login link") do
   expect(page.first(:xpath, "//a[text()='Staff login']"))
 end
+Then /^show me the cookies!$/ do
+  show_me_the_cookies
+end
+
+Then /^show me the "([^"]*)" cookie$/ do |cookie_name|
+  show_me_the_cookie(cookie_name)
+end
+
+Given /^I close my browser \(clearing the session\)$/ do
+  expire_cookies
+end
