7324d07f3229de82cd9c031ead9e2cb9bf1a160c
James Reidy <jreidy@cornell.edu>
2020-07-02 12:30:39 -0400

    whitespace

--- features/support/env.rb
+++ features/support/env.rb
@@ -94,12 +94,12 @@ else # else create driver instance for desktop browser
         Capybara.register_driver :chrome do |app|
           Capybara::Selenium::Driver.new(app, browser: :chrome)
         end
-        
+
         Capybara.register_driver :headless_chrome do |app|
           capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
             chromeOptions: { args: %w(headless disable-gpu) }
           )
-        
+
           Capybara::Selenium::Driver.new app,
             browser: :chrome,
             desired_capabilities: capabilities
